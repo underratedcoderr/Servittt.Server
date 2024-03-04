3c8c7717211e0ec70d168663e3b92ea41d188034
underratedcoderr <155761221+underratedcoderr@users.noreply.github.com>
2024-03-04 19:35:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.get("/logo.png", (req, res) => {
 
 app.post("/razorpay", async (req, res) => {
   const payment_capture = 1;
-  const amount = 10;
+  const amount = 100;
   const currency = "INR"; 
 
   const options = {
